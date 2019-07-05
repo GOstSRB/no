9dabb6b2aa07be6765a8e197ec8e0fad9edd5976
Milan Radic <milan1rns@gmail.com>
2019-07-05 11:12:32 +0200

    no ver1.0

---
+++ src/main/java/no/NoApplication.java
@@ -0,0 +1,17 @@
+package no;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+
+
+@SpringBootApplication
+public class NoApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoApplication.class, args);
+
+	}
+
+}
---
+++ src/main/java/no/model/User.java
@@ -0,0 +1,48 @@
+package no.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tbl_user")
+public class User {
+	
+	@Id
+	@GeneratedValue
+	@Column(name="id")
+	private Long id;
+	
+	@Column(name="username",nullable=false)
+	private String username;
+	
+	@Column(name="password",nullable=false)
+	private String password;
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
